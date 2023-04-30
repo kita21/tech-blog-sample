aebec16059a7f48a90def923bbae954dac0ccef5
kita21 <forsyuvikings@gmail.com>
2023-04-30 23:00:51 +0900

    all tmp

---
+++ all/app/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { Request, Response } from "express";
+const app = express();
+const port = 80;
+app.get('/', (_req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
